82ab2cb26b038be2d18bf1a1eba85111b4e898fb
weibin0516 <codingforfun@126.com>
2019-08-28 08:17:09 +0800

    add docker for kylin

---
+++ entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "127.0.0.1 sandbox.hortonworks.com" >> /etc/hosts
+
+# start mysql
+service mysqld start
+mysqladmin -uroot password 123456
+mysql -uroot -p123456 -e "grant all privileges on root.* to root@'%' identified by '123456';"
+
+# start hdfs
+if [ ! -f "/home/admin/first_run" ]
+then
+    hdfs namenode -format
+fi
+touch /home/admin/first_run
+$HADOOP_HOME/sbin/hadoop-daemon.sh start namenode
+$HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
+
+# start yarn
+$HADOOP_HOME/sbin/yarn-daemon.sh start resourcemanager
+$HADOOP_HOME/sbin/yarn-daemon.sh start nodemanager
+
+# start mr jobhistory
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+
+# start hbase
+$HBASE_HOME/bin/start-hbase.sh
+
+# start kafka
+rm -rf /tmp/kafka-logs
+rm -rf /data/zookeeper/*
+nohup $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
+
+# start kylin
+$KYLIN_HOME/bin/kylin.sh start
+sh $KYLIN_HOME/bin/sample.sh
+
+# prepare kafka topic and data
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic kylin_streaming_topic
+nohup $KYLIN_HOME/bin/kylin.sh org.apache.kylin.source.kafka.util.KafkaSampleProducer --topic kylin_streaming_topic --broker localhost:9092 &
+
+while :
+do
+    sleep 1
+done
